ee821ae937f01b05a4ce098a5f03214c441fa84d
diakt <igs2107@columbia.edu>
2022-08-23 11:51:39 -0400

    project added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,23 +3,32 @@ import React from 'react';
 function Projects() {
     return (
         <div className='PROJECTS-main'>
-            <div class="left-child">
-                
+            <div class="proj-left-child">
+                <p></p>
             </div>
-            
-            <div class="mid-child">
+            <div class="proj-mid-child">
                 <h2 class="projects-title">Projects</h2>
-                <ul class="projects-list">
-                    <article class="project-class">
-                        <h3 class="projects-title">Maybe if I wasn't such a layabout, I'd have more to show you.</h3>
-                        <p>Check back soon.</p>
+                
+                <section class="project-class">
+                    <article className="project-text">
+                        <div className='project-encapsulation'>
+                            <h3 class="project-title">Dangerous Lighting
+                                <a class="project-link" href='https://dangerouslighting.netlify.app'target="_blank"rel="noreferrer"> dangerouslighting.netlify.app</a>
+                            </h3>
+                            <p className="project-description"> Wildfire mapping in real time. Built with a React.js frontend and an Express.js backend, incorporating IRWIN and EONET APIs.</p>
+                        </div>
                     </article>
+                    <img className = "project-image" src={require("../images/dangerous-lighting.png")}/>
+                </section>
+                
+               
+                
                     
-                </ul>
+                
             </div>
 
-            <div class="right-child">
-                
+            <div class="proj-right-child">
+                <p></p>
             </div>
 
 
