8d8c165b5a8fb971fc65fcdce9f58c0321ec61d8
diakt <igs2107@columbia.edu>
2023-02-20 16:58:47 -0800

    finalfooter

--- src/components/NewInterests.js
+++ src/components/NewInterests.js
@@ -4,16 +4,21 @@ function NewInterests(props) {
   return (
     <Container style={{ margin: "7vh auto" }}>
       <Row>
-        {/* <Col style={{ border: "1px red", borderStyle: "solid" }}> */}
-        <Col>
+        <Col className="text-center">
+          {/* <Col> */}
           <img
             className="interest-image"
             src={require("../images/chopper-burn-norcal.jpg")}
             alt="A slithery little friend"
-            style={{ height: "40vh", marginBottom: "2vh" }}
+            style={{
+              height: "40vh",
+            }}
           />
 
-          <p className="interest-text" style={{ width: "75%" }}>
+          <p
+            className="interest-text"
+            style={{ width: "75%", margin: "1vh auto" }}
+          >
             I’m interested in environmental hazards and risk management.
             Wildfire will worsen by several orders of magnitude over the next
             fifty years in the Pacific Northwest and Russia. I enjoy using
@@ -22,15 +27,18 @@ function NewInterests(props) {
           </p>
         </Col>
         {/* <Col style={{ border: "1px red", borderStyle: "solid" }}> */}
-        <Col>
+        <Col className="text-center">
           <img
             className="interest-image"
             src={require("../images/mpb.jpg")}
             alt="A mountain pine beetle"
-            style={{ height: "40vh", marginBottom: "2vh" }}
+            style={{ height: "40vh" }}
           />
 
-          <p className="interest-text" style={{ width: "75%" }}>
+          <p
+            className="interest-text"
+            style={{ width: "75%", margin: "1vh auto" }}
+          >
             I specifically find bark beetles really interesting. The term bark
             beetle isn't a classification as much as a four-letter word for any
             of a class of ~6,000 insects that predate trees. They're currently
@@ -43,30 +51,36 @@ function NewInterests(props) {
       </Row>
       <Row>
         {/* <Col style={{ border: "1px red", borderStyle: "solid" }}> */}
-        <Col>
+        <Col className="text-center">
           <img
             className="interest-image"
             src={require("../images/EARC_heat.JPG")}
             alt="Rowing in a shell, dressed like an idiot."
-            style={{ height: "40vh", marginBottom: "2vh" }}
+            style={{ height: "40vh" }}
           />
 
-          <p className="interest-text" style={{ width: "75%" }}>
+          <p
+            className="interest-text"
+            style={{ width: "75%", margin: "1vh auto" }}
+          >
             I was a decent rower. In large part, I became a math major because
             of an interesting combinatorial optimization problem I encountered
             within the sport.
           </p>
         </Col>
         {/* <Col style={{ border: "1px red", borderStyle: "solid" }}> */}
-        <Col>
+        <Col className="text-center">
           <img
             className="interest-image"
             src={require("../images/EARC-champs.JPG")}
             alt="EARC champions"
-            style={{ height: "40vh", marginBottom: "2vh" }}
+            style={{ height: "40vh" }}
           />
 
-          <p className="interest-text" style={{ width: "75%" }}>
+          <p
+            className="interest-text"
+            style={{ width: "75%", margin: "1vh auto" }}
+          >
             A good day at Eastern Sprints. Not much more to say.
           </p>
         </Col>
